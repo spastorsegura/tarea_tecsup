a348b23e8a1139abf9d60f6bfc1a01c5e427b66b
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-01-24 22:42:05 -0500

    enviar

--- Reto6/opc1/index.js
+++ Reto6/opc1/index.js
@@ -1,6 +1,11 @@
+/* Con HTML, CSS y JS, crea un pequeño Input y un botón, donde al hacer click en el botón guarde el valor del input en un arreglo, no es necesario mostrar el texto guardado en el navegador.
+Tips: usar .push(), .value , addEventListener()
+ */
+
 
 var formulario=document.getElementById("registro");
 var inputTexto=document.getElementById("task");
+var btnSubmit=document.getElementById("btn");
 var arreglo=[];
 
 formulario.addEventListener("submit",function(e){
